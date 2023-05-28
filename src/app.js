6eaee8eada2e3923cbdbe98389ac4a5879227a54
Dacosta <dacsolo10@gmail.com>
2023-05-28 05:16:10 +0000

    deployed

--- src/app.js
+++ src/app.js
@@ -9,25 +9,25 @@ const BookMarkRouter = require("./router/bookmarks.router");
 const app = express();
 
 app.use(cors());
-app.use(
-  helmet({
-    crossOriginResourcePolicy: false,
-  })
-);
-app.use(helmet.crossOriginResourcePolicy({ policy: "cross-origin" }));
+// app.use(
+//   helmet({
+//     crossOriginResourcePolicy: false,
+//   })
+// );
+app.use(helmet());
 app.use(morgan("combined"));
 app.use(express.json());
 app.use("/user", UserRouter);
 app.use("/bookmarks", BookMarkRouter);
 app.use(express.static(path.join(__dirname, "..", "public")));
 
-app.get("/*", (req, res) => {
-  res.sendFile(path.join(__dirname, "..", "public", "index.html"));
-});
-
 app.get("/images/:filename", (req, res) => {
   const { filename } = req.params;
   res.sendFile(path.join(__dirname, ".", "assets", filename));
+});
+
+app.get("/*", (req, res) => {
+  res.sendFile(path.join(__dirname, "..", "public", "index.html"));
 });
 
 module.exports = app;
